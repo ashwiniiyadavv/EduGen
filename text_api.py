d2bfffea2501ac2b9862853330287594edd8437c
RAMKISHOR2001 <rkprajapati1412@gmail.com>
2025-01-11 21:05:08 +0530

    token update

--- text_api.py
+++ text_api.py
@@ -8,7 +8,7 @@ api = os.environ["text_api"]
 def text_api(api):
     url = api
     headers = {
-        'Authorization': 'Bearer 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',
+        'Authorization': 'Bearer hf_xxxxxxxxxxxxxxxxxxxx',
         'Content-Type': 'application/json'
     }
     data = {
